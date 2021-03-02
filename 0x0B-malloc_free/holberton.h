dc35a4461306336560f68e5911bc6d6739343268
Carlo Ertola <carloertola@gmail.com>
2021-03-02 15:06:09 +0000

    updated header file

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -8,5 +8,6 @@ void *malloc(size_t size);
 void free(void *ptr);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 
 #endif /* HOLBERTON_H */
