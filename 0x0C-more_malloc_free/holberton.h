7e0c98cd8798e33f59629d0e5a95c4982f11171e
Carlo Ertola <carloertola@gmail.com>
2021-03-04 09:13:00 +0000

    updated header file

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -9,5 +9,6 @@ void free(void *ptr);
 void exit(int status);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif /* HOLBERTON_H */
