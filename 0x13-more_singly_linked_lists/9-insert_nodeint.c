da03e91fbf9f20f4bd4d1db358d90bedc53be1b9
Carlo <info@carloertola.com>
2021-03-23 14:24:16 +0000

    added check for NULL parameter

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node = malloc(sizeof(listint_t)), *temp, *prev_node;
 	unsigned int i = 0;
 
-	if (!new_node)
+	if (!new_node || !head)
 	{
 		free(new_node);
 		return (NULL);
