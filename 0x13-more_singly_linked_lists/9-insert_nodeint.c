9a0026da8eed1c453ba16caafd1999900b311fce
Carlo <info@carloertola.com>
2021-03-23 14:22:07 +0000

    fixed one conditional

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new_node->next = temp;
 		return (new_node);
 	}
-	while (temp->next)
+	while (temp)
 	{
 		if (i == idx)
 		{
