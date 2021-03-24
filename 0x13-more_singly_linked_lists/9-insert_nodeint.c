a1f64ec9f16552699e967bbdf91f346a8a71eaa7
Carlo <info@carloertola.com>
2021-03-24 17:28:07 +0000

    Tried a different approach

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,27 +10,28 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node = malloc(sizeof(listint_t)), *temp;
+	listint_t *new_node, *temp;
 	unsigned int i = 0;
 
-	if (!new_node || !head)
-	{
-		free(new_node);
+	if (!head)
 		return (NULL);
-	}
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 	new_node->n = n;
-	temp = *head;
 	if (idx == 0)
 	{
-		new_node->next = temp;
-		temp = new_node;
+		new_node->next = *head;
+		*head = new_node;
 		return (new_node);
 	}
-	while (i < idx - 1)
+	temp = *head;
+	while (i < (idx - 1))
 	{
 		temp = temp->next;
 		i++;
-		if (!temp)
+		if (temp == NULL)
 		{
 			free(new_node);
 			return (NULL);
