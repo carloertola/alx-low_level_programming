5f8fc4c74bf725b521ec57b0d04039b82a4b6afe
Carlo <info@carloertola.com>
2021-03-24 17:21:52 +0000

    removed a condition from my while statement

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = new_node;
 		return (new_node);
 	}
-	while (temp && i < idx - 1)
+	while (i < idx - 1)
 	{
 		temp = temp->next;
 		i++;
