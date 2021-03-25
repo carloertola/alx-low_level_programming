19d02326e787911b124cc8811d9b1492cdcd697c
Carlo Ertola <info@carloertola.com>
2021-03-25 18:07:24 +0300

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,8 +9,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-        if (index > 8 * 32 || !n)
-                return (-1);
-        *n |= 1UL << index;
+	if (index > 8 * 32 || !n)
+		return (-1);
+	*n |= 1UL << index;
         return (1);
 }
