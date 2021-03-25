6460f57f24e7fabc3fa7323572c5168cc27dcf75
Carlo <info@carloertola.com>
2021-03-25 08:53:09 +0000

    added README file

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *prev_node, *current_node = *head, *next_node;
 	unsigned int i = 0;
 
-	if (!head)
+	if (head == NULL)
 		return (-1);
 	next_node = current_node->next;
 	if (index == i && !current_node)
