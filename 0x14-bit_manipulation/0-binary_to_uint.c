c09c7a02255c61e338db3bbde6178b905a356824
Carlo <info@carloertola.com>
2021-03-25 11:53:26 +0000

    simplified the code using bit operators

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,16 +10,15 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0, dec = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 	while (b[i])
 	{
 		if (!(b[i] == '0' || b[i] == '1'))
 			return (0);
-		if (i == 0)
-			dec = b[i] - '0';
-		else
-			dec = 2 * dec + (b[i] - '0');
+		dec <<= 1;
+		if (b[i] == '1')
+			dec += 1;
 		i++;
 	}
 	return (dec);
