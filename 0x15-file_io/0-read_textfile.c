474cff6e644eb0aec1409d27504ba8ab2e7e1121
carloertola <info@carloertola.com>
2021-03-29 10:07:45 +0000

    used write as opposed to _putchar.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,24 +1,6 @@
 #include "holberton.h"
 
 /**
- * print_string - prints a string using _putchar
- *
- * @str: pointer to string
- * Return: characters printed
- */
-ssize_t print_string(char *str)
-{
-	ssize_t chars_printed = 0;
-
-	while (str[chars_printed])
-	{
-		_putchar(str[chars_printed]);
-		chars_printed++;
-	}
-	return (chars_printed);
-}
-
-/**
  * read_textfile - reads a text file and prints it to the POSIX standard output
  *
  * @filename: file name
@@ -27,32 +9,23 @@ ssize_t print_string(char *str)
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, rd;
-	ssize_t chars_printed = 0;
-	char *buffer = malloc(sizeof(char) * (letters + 1));
+	int fd, b;
+	char *buffer;
 
-	if (!buffer)
+	if (!filename || letters < 1)
 		return (0);
-	buffer[letters] = '\0';
-	if (!filename)
-	{
-		free(buffer);
-		return (0);
-	}
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	b = read(fd, buffer, letters);
+	if (b == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-	rd = read(fd, buffer, letters);
-	if (rd == -1)
-	{
-		free(buffer);
-		return (0);
-	}
-	chars_printed = print_string(buffer);
+	write(STDOUT_FILENO, buffer, b);
 	close(fd);
 	free(buffer);
-	return (chars_printed);
+	return (b);
 }
