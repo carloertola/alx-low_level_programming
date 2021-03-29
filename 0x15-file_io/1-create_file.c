02d3380055621ac654a3fb93d22e2912e70d09e8
carloertola <info@carloertola.com>
2021-03-29 11:39:53 +0000

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd = open(filename, O_CREAT | O_WRONLY, 0600), count = 0;
+	int fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600), count = 0;
 
 	if (!filename || fd == -1)
 		return (-1);
